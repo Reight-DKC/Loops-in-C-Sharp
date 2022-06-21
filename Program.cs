6c5c6bc5f1906c7c5a4ab8b85c14d42ed1291d4a
Reight-DKC <reightultimate@gmail.com>
2022-06-20 19:45:37 -0600

    Loops practice done in class complete

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("C# Loops!");
+Console.WriteLine("Printing pairs between two numbers\n");
+// { o }
+Console.WriteLine("Enter the first number: ");
+int firstNumber = Int32.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Enter the second number: ");
+int secondNumber = Int32.Parse(Console.ReadLine() ?? "1000");
+
+for (int i = firstNumber; i <= secondNumber; i++)
+{
+    if (i % 2 == 0)
+  {
+    Console.WriteLine(i);
+  }
+}
+
+Console.WriteLine("\nEnd of program....");
